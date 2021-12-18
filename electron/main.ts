08452064868ed874f26a3ed7d66d1f4c95f2f464
Eoin Bathurst <silentechouk@gmail.com>
2021-12-18 19:46:45 +0100

    clean up

--- electron/main.ts
+++ electron/main.ts
@@ -14,6 +14,7 @@ const createWindow = () => {
     },
     autoHideMenuBar: true,
     roundedCorners: true,
+    title: "SOS Emulator",
   });
 
   win.loadURL(dev ? "http://localhost:3000" : "http://www.google.com");
--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -3,8 +3,6 @@
   import { getLogger } from "$lib/logger";
 
   const log = getLogger({ filepath: "svelte/src/routes/index.svelte" });
-
-  $: log.info("Frontend socket connected to backend:", socket.connected);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
