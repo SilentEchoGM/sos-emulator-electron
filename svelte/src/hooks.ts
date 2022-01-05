7711916c652ac5ebece7ae51e9371c045fa4dc75
Eoin Bathurst <silentechouk@gmail.com>
2022-01-05 19:57:16 +0100

    cleaned up logging

--- svelte/src/hooks.ts
+++ svelte/src/hooks.ts
@@ -1,8 +1,6 @@
-import { httpServer, startSOSEmulator } from "$lib/backend/socket";
+import { startSOSEmulator } from "$lib/backend/socket";
 import { getLogger } from "$lib/logger";
-import stringify from "fast-safe-stringify";
 
 const log = getLogger({ filepath: "svelte/src/hooks.ts" });
 
-log.info("Hooks!", stringify(httpServer));
 startSOSEmulator();
